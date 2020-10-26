d24b54c3fd746b9295a99a098c3b2b9d4166adde
Sam Higson <sh961@exeter.ac.uk>
2020-10-26 23:21:24 +0000

    Started Updating: added main method with n input
    Added main method for CardGame

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class CardGame {
+  public static void main(String[] args) {
+    // main method for entire game simulation
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please Enter Number of Players:");
+
+    int n = input.nextInt();
+    System.out.println("Setting Up A " + n + " Player Game");
+  }
 }
