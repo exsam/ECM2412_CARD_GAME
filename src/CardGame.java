88ff1e9ff43f942950c1045b43241c964c834767
Sam Higson <sh961@exeter.ac.uk>
2020-10-26 23:59:23 +0000

    Input Cleaned

--- src/CardGame.java
+++ src/CardGame.java
@@ -4,7 +4,7 @@ public class CardGame {
   public static void main(String[] args) {
     // main method for entire game simulation
     Scanner input = new Scanner(System.in);
-    System.out.println("Please Enter Number of Players:");
+    System.out.print("Please Enter Number of Players: ");
 
     int n = input.nextInt();
     System.out.println("Setting Up A " + n + " Player Game");
