f1999fb3ab0425611188f5c7da03dfb490bed15c
rw600 <61245286+rw600@users.noreply.github.com>
2020-10-30 23:23:32 +0000

    Update CardDeck.java
    Added basic code to dynamically populate the decks using a 2d arraylist.

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+
 public class CardDeck {
-    public synchronized void addCard() {
+	
+	public static ArrayList<ArrayList<Integer>> Deck; 
+	
+	public static void initialiseDeck (int playerCount) {
+		ArrayList<ArrayList<Integer>> tempDeck = new ArrayList<>(playerCount);
+		for(int i=0; i < playerCount; i++) {
+			tempDeck.add(new ArrayList<Integer>());
+		}
+		Deck = tempDeck;
+	}
+	
+	public static void outputDeck() {
+		System.out.println(Deck);
+	}
+	
+    public static synchronized void addCard(int deckNumber, int cardDesignation) {	
+    	System.out.println("Adding: " + cardDesignation + " to Deck: " + deckNumber);
+    	Deck.get(deckNumber).add(cardDesignation);
 
     }
-    public synchronized int drawCard() {
+    
+    public synchronized int drawCard(int deckIndex, int cardDesignation) {
         return 0;
     }
 }
